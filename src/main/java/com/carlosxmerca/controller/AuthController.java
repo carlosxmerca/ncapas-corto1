dae0feb974f93513d0b02389c1320440dda49966
carlosxmerca <eduarmercado4@gmail.com>
2023-03-29 22:23:46 -0600

    Update routes: 404, fix: controller methods

--- src/main/java/com/carlosxmerca/controller/AuthController.java
+++ src/main/java/com/carlosxmerca/controller/AuthController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class AuthController {
 	
 	private static final List<Employee> users = new ArrayList<>();
+	private static Employee user = null;
 	private static final boolean ACTIVE = true;
 	private static final boolean INACTIVE = false;
 	private static final boolean ADMIN = true;
@@ -36,11 +37,7 @@ public class AuthController {
     }
     
     @PostMapping("/auth")
-	public String login(@ModelAttribute EmployeeDTO credentials, Model model, HttpSession session) {
-    	String time = Calendar.getInstance().getTime().toString();
-    	model.addAttribute("time", time);
-    	model.addAttribute("users", users);
-
+	public String login(@ModelAttribute EmployeeDTO credentials) {
 		Employee employee = users.stream()
 				.filter(u -> u.getCode().equals(credentials.getCode()) && u.getPass().equals(credentials.getPass()))
 				.findFirst()
@@ -48,22 +45,43 @@ public class AuthController {
 
 
 		if (employee == null || !employee.isActive())
-			return "404";
+			return "redirect:/404";
 		
 		try {
 			Date currentDate = new Date();
 			Date hireDate = new SimpleDateFormat("dd/MM/yyyy").parse(employee.getDateOfHire());
 			if (hireDate.compareTo(currentDate) > 0) {
-				return "404";
+				return "redirect:/404";
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		
-		model.addAttribute("user", employee.getNames());
-		//session.setAttribute("employee", employee);
-		return "/dashboard";
+		user = employee;
+		
+		return "redirect:/dashboard";
 	}
+    
+    @GetMapping("/logout")
+    public String logout() {
+    	user = null;
+    	return "redirect:/login";
+    }
+    
+    @GetMapping("/dashboard")
+	public String getDashboard(Model model) {
+    	String time = Calendar.getInstance().getTime().toString();
+    	model.addAttribute("time", time);
+    	model.addAttribute("users", users);
+    	
+    	if (user == null)
+    		return "redirect:/404";
+    	    	
+    	model.addAttribute("user", user.getNames());
+    	model.addAttribute("isUserAdmin", user.isAdmin());
+    	
+    	return "dashboard";
+    }
 	
 /*
 	@GetMapping("/dashboard")
@@ -80,10 +98,15 @@ public class AuthController {
 		}
     }
 */
-	
+        
 	@GetMapping("/login")
     public String getLogin() {
     	return "login";
+    }
+	
+	@GetMapping("/404")
+    public String get404() {
+    	return "not-found";
     }
 	
 }
