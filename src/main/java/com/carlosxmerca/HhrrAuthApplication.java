1bdde19e70bbd34febe0613bd1fad83d807ddc73
carlosxmerca <eduarmercado4@gmail.com>
2023-03-29 18:51:44 -0600

    Init project

---
+++ src/main/java/com/carlosxmerca/HhrrAuthApplication.java
@@ -0,0 +1,13 @@
+package com.carlosxmerca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhrrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhrrAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosxmerca/HhrrAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosxmerca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhrrAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
